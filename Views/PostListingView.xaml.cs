b000d29f179d82e81bc82fa86f2ed047aaea78cf
UlysseLBT <lebouteiller.ulysse@gmail.com>
2026-03-02 09:47:37 +0100

    Remplace UserListingView par PostListingView dans MainWindow
    MainWindow affiche désormais PostListingView à la place de UserListingView.
    Le DataContext de PostListingView est explicitement initialisé avec PostListingViewModel.
    Nettoyage des using inutilisés dans PostListingView.xaml.cs.
    Le SDK du projet passe à Microsoft.NET.Sdk (WPF toujours activé).

--- Views/PostListingView.xaml.cs
+++ Views/PostListingView.xaml.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using MiniYou_App.ViewModels;
 
 namespace MiniYou_App.Views
 {
-    /// <summary>
-    /// Logique d'interaction pour PostListingView.xaml
-    /// </summary>
     public partial class PostListingView : UserControl
     {
         public PostListingView()
         {
-
             InitializeComponent();
+            DataContext = new PostListingViewModel();
         }
     }
 }
